676631eb6c44419336cd4353abb181b8941085a6
Jannetty <jannetty@uw.edu>
2021-11-29 15:23:51 -0800

    Added more functionality. Still not working.

--- src/pval_module/stattest_clusters_approx.py
+++ src/pval_module/stattest_clusters_approx.py
@@ -5,11 +5,32 @@ clusterpval website: https://www.lucylgao.com/clusterpval/
 Package described in: https://arxiv.org/abs/2012.02936
 """
 
-import numpy as np
 import math
+import scipy
+import numpy as np
+import pandas as pd
 
-def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun, positional_arguments, keyword_arguments,
-                             iso=True, sig=None, SigInv=None, ndraws=1):
+
+def preserve_cl(cl, cl_phi, k1, k2):
+    """
+    Checks if Ck, Ck' in C(x'(phi)). Returns True if Ck, Ck' in (C(x'(phi))),
+    False otherwise. Checks if clusters in original cluster labelset are the
+    same size as clusters in perturbed data labelset
+    :param cl: list or numpy.ndarray, cluster labels from initial input
+    :param cl_phi: list or numpy.ndarray, cluster labels of perturbed data
+    :param k1: integer, index of cluster involved in test
+    :param k2: integer, intex of cluster involved in test
+    :return: True if Ck, Ck' in (C(x'(phi))), False otherwise
+    """
+    df = pd.DataFrame({'cl':cl, 'cl_phi':cl_phi})
+    tab = pd.crosstab(index=df['cl'], columns=df['cl_phi'])
+    k1_in = (sum(tab.iloc[k1, :] != 0) == 1) and (sum(tab.iloc[:, k1] != 0) == 1)
+    k2_in = (sum(tab.iloc[k2, :] != 0) == 1) and (sum(tab.iloc[:, k2] != 0) == 1)
+    return k1_in and k2_in
+
+def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
+                             positional_arguments, keyword_arguments,
+                             iso=True, sig=None, SigInv=None, ndraws=200):
     """
     Monte-Carlo significance test for any clustering method. This function takes matrix X clustered
     into K clusters and tests the null hypothesis of no difference in means between clusters k1 and
@@ -20,6 +41,9 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun, positional_argum
     :param k1: integer, selects a cluster to test
     :param k2: integer, selects a cluster to test
     :param cluster_labels: numpy.ndarray, labels of each point (row) in X
+    :param cl_fun: function used to cluster data in clustering module
+    :param positional_arguments: list of positional arguments used by cl_fun
+    :param keyword_arguments: dict of keyword argument key:value pairs used by cl_fun
     :param iso: boolean, if True isotropic covariance matrix model, otherwise not
     :param sig: optional scalar specifying sigma,  relevant if iso == True
     :param SigInv: optional matrix specifying Sigma^-1, relevant if iso == False
@@ -83,7 +107,8 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun, positional_argum
     orig_k2 = np.transpose(X[cluster_labels == k2, :])
     Xphi = X
 
-    ndraws_above_0 = []
+    num_preserved = 0
+
     for j in range(ndraws):
         if phi[j] < 0:
             continue
@@ -95,4 +120,22 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun, positional_argum
         #recluster the perturbed data set
         cl_Xphi = cl_fun(*positional_arguments, **keyword_arguments)
         cl_Xphi.fit_predict(Xphi)
-        print(cl_Xphi.labels_)
+
+        if(preserve_cl(cluster_labels, cl_Xphi.labels_, k1, k2)):
+            num_preserved += 1
+            first_term = -((phi[j]/scale_factor)**2)/2 + \
+                         (q-1)*math.log(phi[j]/scale_factor)
+            #print(first_term)
+            middle_term = (q/2 - 1)*math.log(2) - math.log(math.gamma(q/2)) -\
+                          math.log(scale_factor)
+            #print(middle_term)
+            last_term = scipy.stats.norm.logpdf(phi[j], loc=stat,
+                                                scale=scale_factor)
+            #print(last_term)
+            log_survives[j] = first_term - middle_term - last_term
+
+    #trim down to only survives
+    print("num preserved = {}".format(num_preserved))
+    print(sum(value is not None for value in log_survives))
+    phi = phi[log_survives != None]
+    print(len(phi[log_survives != None][0]))
