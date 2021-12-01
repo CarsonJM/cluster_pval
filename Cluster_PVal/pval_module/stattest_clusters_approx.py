2bb3708900f96e98c7c9aa0cd74f8eee89ded44e
Jannetty <jannetty@uw.edu>
2021-12-01 13:48:35 -0800

    Implemented Wald test. Formatted stattest. Added Try/Excepts to some unit tests

--- Cluster_PVal/pval_module/stattest_clusters_approx.py
+++ Cluster_PVal/pval_module/stattest_clusters_approx.py
@@ -1,7 +1,7 @@
 """
 Implements Lucy L Gao's test_clusters_approx function from clusterpval R package
 Code for original function found in clusterpval/R/trunc_inf.R
-clusterpval website: https://www.lucylgao.com/clusterpval/
+clusterpval package website: https://www.lucylgao.com/clusterpval/
 Package described in: https://arxiv.org/abs/2012.02936
 """
 
@@ -28,16 +28,16 @@ def preserve_cl(cl, cl_phi, k1, k2):
     k2_in = (sum(tab.iloc[k2, :] != 0) == 1) and (sum(tab.iloc[:, k2] != 0) == 1)
     return k1_in and k2_in
 
-def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
+def stattest_clusters_approx(x, k1, k2, cluster_labels, cl_fun,
                              positional_arguments, keyword_arguments,
-                             iso=True, sig=None, SigInv=None, ndraws=2000):
+                             iso=True, sig=None, siginv=None, ndraws=2000):
     """
     Monte-Carlo significance test for any clustering method. This function takes matrix X clustered
     into K clusters and tests the null hypothesis of no difference in means between clusters k1 and
     k2. To account for the fact that the clusters were estimated from the data, the p-values are
     computed conditional on the fact that those clusters were estimated. P-values are approximated
     via importance sampling.
-    :param X: n by p matrix, containing numeric data
+    :param x: n by p matrix (np.array), containing numeric data
     :param k1: integer, selects a cluster to test
     :param k2: integer, selects a cluster to test
     :param cluster_labels: numpy.ndarray, labels of each point (row) in X
@@ -46,7 +46,7 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
     :param keyword_arguments: dict of keyword argument key:value pairs used by cl_fun
     :param iso: boolean, if True isotropic covariance matrix model, otherwise not
     :param sig: optional scalar specifying sigma,  relevant if iso == True
-    :param SigInv: optional matrix specifying Sigma^-1, relevant if iso == False
+    :param siginvigInv: optional matrix specifying Sigma^-1, relevant if iso == False
     :param ndraws: integer, selects the number of importance samples, default of 2000
     :return:
         - stat - float, the test statistic: Euclidean distance between mean of cluster k1 and mean of
@@ -56,7 +56,7 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
     """
 
     #check to make sure X is 2D ndarray
-    rows, cols = X.shape
+    rows, cols = x.shape
     n = rows
     q = cols
 
@@ -69,15 +69,15 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
     n1 = points_per_cluster[k1]
     n2 = points_per_cluster[k2]
     squared_norm_nu = (1/n1) + (1/n2)
-    k1colmeans = np.mean(X[cluster_labels == k1,:], axis=0)
-    k2colmeans = np.mean(X[cluster_labels == k2,:], axis=0)
+    k1colmeans = np.mean(x[cluster_labels == k1,:], axis=0)
+    k2colmeans = np.mean(x[cluster_labels == k2,:], axis=0)
     diff_means = k1colmeans - k2colmeans
     prop_k2 = n2/(n1+n2)
 
     if iso == True:
         if sig == None:
-            colmeans = np.mean(X, axis=0)
-            scaledX = X - colmeans
+            colmeans = np.mean(x, axis=0)
+            scaledX = x - colmeans
             sig = math.sqrt(np.sum(scaledX**2) / (n*q - q))
         else:
             pass
@@ -85,11 +85,11 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
         #Compute test statistic
         stat = np.linalg.norm(diff_means)
     else:
-        if SigInv == None:
-            colmeans = np.mean(X, axis=0)
-            scaledX = X - colmeans
-            Sig = np.cov(scaledX, rowvar=False)
-            SigInv = np.linalg.inv(Sig)
+        if siginv == None:
+            colmeans = np.mean(x, axis=0)
+            scaledX = x - colmeans
+            sig = np.cov(scaledX, rowvar=False)
+            SigInv = np.linalg.inv(sig)
         else:
             pass
         scale_factor = squared_norm_nu
@@ -102,15 +102,15 @@ def stattest_clusters_approx(X, k1, k2, cluster_labels, cl_fun,
     phi = np.random.normal(size=ndraws)*scale_factor + stat
     k1_constant = prop_k2*diff_means/stat
     k2_constant = (prop_k2 - 1)*diff_means/stat
-    orig_k1 = np.transpose(X[cluster_labels == k1,:])
-    orig_k2 = np.transpose(X[cluster_labels == k2, :])
-    Xphi = X
+    orig_k1 = np.transpose(x[cluster_labels == k1,:])
+    orig_k2 = np.transpose(x[cluster_labels == k2, :])
+    Xphi = x
 
     for j in range(ndraws):
         if phi[j] < 0:
             continue
         #compute the perturbed data set
-        Xphi = X
+        Xphi = x
         Xphi[cluster_labels == k1, :] = np.transpose(orig_k1[:]) + ((phi[j] - stat) * k1_constant)
         Xphi[cluster_labels == k2, :] = np.transpose(orig_k2[:]) + ((phi[j] - stat) * k2_constant)
         #recluster the perturbed data set
--- Cluster_PVal/tests/test_pvalmod.py
+++ Cluster_PVal/tests/test_pvalmod.py
@@ -3,22 +3,19 @@ Tests for pval module
 """
 
 import numpy as np
-import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 import unittest
-
-from scipy.cluster.hierarchy import dendrogram, linkage
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 from pval_module.stattest_clusters_approx import \
     stattest_clusters_approx
-
+from pval_module.wald_test import wald_test
 
 class TestPvalModule(unittest.TestCase):
 
-    def test_smoke(self):
+    def test_smoke_stattest_clusters_approx(self):
         """
-        simple smoke test to make sure function runs
+        simple smoke test to make sure stattest_clusters_approx function runs
         :return: nothing
         """
         x = np.array([[5, 3],
@@ -31,19 +28,89 @@ class TestPvalModule(unittest.TestCase):
                       [60, 78],
                       [70, 55],
                       [80, 91], ])
-        cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+        K = 2
+        cl_fun = AgglomerativeClustering
+        positional_arguments = []
+        keyword_arguments = {'n_clusters': K, 'affinity': 'euclidean',
+                             'linkage': 'average'}
+        cluster = cl_fun(*positional_arguments, **keyword_arguments)
         cluster.fit_predict(x)
-        #plt.scatter(x[:, 0], x[:, 1], c=cluster.labels_, cmap='rainbow')
-        #plt.show()
         k1 = 0
         k2 = 1
-        stattest_clusters_approx(x, k1, k2, cluster.labels_)
+        stattest_clusters_approx(x, k1, k2, cluster.labels_, cl_fun,
+                             positional_arguments, keyword_arguments,)
 
+    def test_smoke_wald_test(self):
+        """
+        simple smoke test to make sure wald_test function runs
+        :return: nothing
+        """
+        x = np.array([[5, 3],
+                      [10, 15],
+                      [15, 12],
+                      [24, 10],
+                      [30, 30],
+                      [85, 70],
+                      [71, 80],
+                      [60, 78],
+                      [70, 55],
+                      [80, 91], ])
+        K = 2
+        cl_fun = AgglomerativeClustering
+        positional_arguments = []
+        keyword_arguments = {'n_clusters': K, 'affinity': 'euclidean',
+                             'linkage': 'average'}
+        cluster = cl_fun(*positional_arguments, **keyword_arguments)
+        cluster.fit_predict(x)
+        k1 = 0
+        k2 = 1
+        wald_test(x, k1, k2, cluster.labels_)
 
-    def test_penguin(self):
+    def test_penguin_gao(self):
         """
         Test using Penguin data used in R tutorial
         :return: same results as shown in R tutorial
+        """
+        penguin_data = np.genfromtxt(
+            'tests/data_for_tests/penguin_data_subset.txt',
+            delimiter=' ', skip_header=1)
+        K = 5
+        #set linkage to average to match R script
+        positional_arguments = []
+        keyword_arguments = {'n_clusters':K, 'affinity':'euclidean', 'linkage':'average'}
+        cluster = AgglomerativeClustering(**keyword_arguments)
+        cluster.fit_predict(penguin_data)
+        #flipped these axes to match figure in R
+        #plt.scatter(penguin_data[:, 1], penguin_data[:, 0],
+        # c=cluster.labels_, cmap='rainbow')
+        #print (cluster.labels_)
+        #plt.show()
+        k1 = 0
+        k2 = 1
+        stat, pval, stderr = stattest_clusters_approx(penguin_data, k1, k2,
+                                          cluster.labels_, AgglomerativeClustering, positional_arguments,
+                                 keyword_arguments, ndraws=10000)
+        passing = True
+        assert np.isclose(stat, 10.11433)
+        try:
+            assert np.isclose(stderr, 0.01084133, atol=.001)
+        except AssertionError:
+            passing = False
+            print("stderr is {}, meant to be within .001 of "
+                  "0.01084133".format(stderr))
+        try:
+            assert np.isclose(pval, 0.6360161, atol=.02)
+        except AssertionError:
+            passing = False
+            print("pval is {}, meant to be within .02 of "
+                  "0.6360161".format(pval))
+        assert(passing == True)
+
+    def test_penguin_wald(self):
+        """
+        Test using Penguin data used in R tutorial
+        :return: same results as shown when using R wald_test function,
+        stat =  10.11433; pval = 0.006226331
         """
         penguin_data = np.genfromtxt('tests/data_for_tests/penguin_data_subset.txt', delimiter=' ',
                                      skip_header=1)
@@ -58,9 +125,6 @@ class TestPvalModule(unittest.TestCase):
         #plt.show()
         k1 = 0
         k2 = 1
-        stat, pval, stderr = stattest_clusters_approx(penguin_data, k1, k2,
-                                          cluster.labels_, AgglomerativeClustering, positional_arguments,
-                                 keyword_arguments, ndraws=10000)
-        print("stat: {}".format(stat))
-        print("pval: {}".format(pval))
-        print("stderr: {}".format(stderr))
+        stat, pval = wald_test(penguin_data, k1, k2, cluster.labels_)
+        assert np.isclose(stat, 10.11433)
+        assert np.isclose(pval, 0.006226331)
