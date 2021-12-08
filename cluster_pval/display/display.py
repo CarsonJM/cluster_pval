c60ea74e5251b899638e3e21f4ef06f2cfac9ae8
Sarah Pollack <sarahpollack1@gmail.com>
2021-12-08 12:41:40 -0800

    Added exception for wrong datatype input

--- cluster_pval/display/display.py
+++ cluster_pval/display/display.py
@@ -8,6 +8,7 @@ from cluster_pval import pval_module
 from cluster_pval import cluster_module
 import plotly.express as px
 
+
 def cluster_plot(clustered_df):
     """
     Function to plot first two principal components of clustered data
@@ -16,17 +17,23 @@ def cluster_plot(clustered_df):
     :param clustered_df: pandas dataframe of clustered RNA seq data
     :return: fig: two-dimensional scatterplot of dimensionally reduced data color-coded by cluster
     """
-    #perform PCA and reduce data to two dimensions
+    # check for correct datatype
+    check_value_type = isinstance(clustered_df, pd.DataFrame)
+    if check_value_type is False:
+        raise ValueError("The dataset should be a pandas dataframe.")
+
+    # perform PCA and reduce data to two dimensions
     pca_2 = PCA(n_components=2)
     principal_components = pca_2.fit_transform(clustered_df)
-    principal_df = pd.DataFrame(data = principal_components, columns = ['first principal component', 'second principal component'])
+    principal_df = pd.DataFrame(data=principal_components,
+                                columns=['first principal component', 'second principal component'])
 
-    #plot data in scatterplot
+    # plot data in scatterplot
     clustered_df = clustered_df.sort_values(['cluster'], ascending=True)
     clustered_df['cluster'] = clustered_df['cluster'].astype(str)
     fig = px.scatter(x=principal_df[:, 0], y=principal_df[:, 1], color=clustered_df['cluster'],
-    labels={'x': "First Principal Component", 'y': "Second Principal Component", 'color': "Cluster"},
-    title="Scatter plot of clustered cells", 
-    template="simple_white")
+                     labels={'x': "First Principal Component", 'y': "Second Principal Component", 'color': "Cluster"},
+                     title="Scatter plot of clustered cells",
+                     template="simple_white")
 
     return fig
