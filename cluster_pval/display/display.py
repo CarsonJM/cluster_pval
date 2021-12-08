f37c0bb1e5fdc080dd7461987c99e0ab6e118f2f
Sarah Pollack <sarahpollack1@gmail.com>
2021-12-07 16:36:37 -0800

    Changed dimensionality reduction from umap to sklearn PCA

--- cluster_pval/display/display.py
+++ cluster_pval/display/display.py
@@ -1,29 +1,22 @@
 """
-Display
-=======
-
-Functions
----------
+Module containing function for plotting clustered data onto a two-dimensional scatterplot using
+principal component analysis
 """
-import umap
+import pandas as pd
+from sklearn.decomposition import PCA
+from cluster_pval import pval_module
 from cluster_pval import cluster_module
 import plotly.express as px
 
 def cluster_plot(clustered_df):
-    # pca2 = PCA(n_components=2)
-    # principalComponents = pca2.fit_transform(X_Scale)
-
-    # principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
-
-    # finalDf = pd.concat([principalDf, df[['class']]], axis = 1)
-    # finalDf.head()
-
-    standard_embedding = umap.UMAP(random_state=42).fit_transform(clustered_df)
+    pca_2 = PCA(n_components=2)
+    principal_components = pca_2.fit_transform(clustered_df)
+    principal_df = pd.DataFrame(data = principal_components, columns = ['first principal component', 'second principal component'])
 
     clustered_df = clustered_df.sort_values(['cluster'], ascending=True)
     clustered_df['cluster'] = clustered_df['cluster'].astype(str)
-    fig = px.scatter(x=standard_embedding[:, 0], y=standard_embedding[:, 1], color=clustered_df['cluster'], 
-    labels={'x': "UMAP_1", 'y': "UMAP_2", 'color': "Cluster"}, 
+    fig = px.scatter(x=principal_df[:, 0], y=principal_df[:, 1], color=clustered_df['cluster'],
+    labels={'x': "First Principal Component", 'y': "Second Principal Component", 'color': "Cluster"},
     title="Scatter plot of clustered cells", 
     template="simple_white")
 
