2d137c6ba97a59ae13e2dea03b647b3a781ff5c1
Sarah Pollack <sarahpollack1@gmail.com>
2021-12-07 16:44:01 -0800

    PEP8 compliant docstrings and comments

--- cluster_pval/display/display.py
+++ cluster_pval/display/display.py
@@ -9,10 +9,19 @@ from cluster_pval import cluster_module
 import plotly.express as px
 
 def cluster_plot(clustered_df):
+    """
+    Function to plot first two principal components of clustered data
+
+    Parameters:
+    :param clustered_df: pandas dataframe of clustered RNA seq data
+    :return: fig: two-dimensional scatterplot of dimensionally reduced data color-coded by cluster
+    """
+    #perform PCA and reduce data to two dimensions
     pca_2 = PCA(n_components=2)
     principal_components = pca_2.fit_transform(clustered_df)
     principal_df = pd.DataFrame(data = principal_components, columns = ['first principal component', 'second principal component'])
 
+    #plot data in scatterplot
     clustered_df = clustered_df.sort_values(['cluster'], ascending=True)
     clustered_df['cluster'] = clustered_df['cluster'].astype(str)
     fig = px.scatter(x=principal_df[:, 0], y=principal_df[:, 1], color=clustered_df['cluster'],
