127b2efc3d2deb3b666a7d49b7fc126d4cd0a81f
CarsonJM <carsonjmiller@outlook.com>
2021-12-09 09:56:19 -0800

    Initial app boilerplate

--- cluster_pval/helper_module/helper_functions.py
+++ cluster_pval/helper_module/helper_functions.py
@@ -19,9 +19,9 @@ from cluster_pval import display_module
 
 import dash
 from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table
+from dash import dcc
+from dash import html
+from dash import dash_table
 
 
 
