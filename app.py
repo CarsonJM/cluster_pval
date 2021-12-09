b6ff5502bbb986240af6a74ae047d104dfe1aab4
CarsonJM <carsonjmiller@outlook.com>
2021-12-09 09:48:54 -0800

    Initial app boilerplate

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import io
 
 import dash
 from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table
+from dash import dcc
+from dash import html
+from dash import dash_table
 
 import pandas as pd
 import plotly.express as px
