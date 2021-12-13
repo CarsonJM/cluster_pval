84cc4e503bf115ec31d2bb239ffac1dcb4c74d1c
Lieke Droog <lyadroog@uw.edu>
2021-12-13 09:39:48 -0800

    Pep8 compliant using pylint

--- cluster_pval/cluster_module/cluster_function.py
+++ cluster_pval/cluster_module/cluster_function.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 29 15:01:35 2021
+""" Implements cluster function for hierarchical and kmeans clustering of a
+pandas dataframe using Scikit designed to cluster RNA seq data, but could be
+applied to other types of datasets too.
 
-Cluster function for hierarchical clustering of a pandas dataset based on requested number of clusters
+The user is requested to submit the dataset, the desired number of clusters,
+the cluster method and linkage method, which is standard set on ward.
+
 """
 
 import pandas as pd
 from sklearn.cluster import AgglomerativeClustering, KMeans
 
 
-def hierarchical_clustering(input_dataset, nr_of_clusters, cluster_method, linkage_method='ward'):
+def clustering(input_dataset, nr_of_clusters, cluster_method,
+                            linkage_method='ward'):
     """
-    Function to cluster data hierarchically
-    
+    Function to cluster data based on number of clusters, cluster method and
+    linkage method. The function automatically tests whether the input data
+    is submitted in the correct data format.
+
     Parameters:
     :param dataset: pandas dataframe, dataset with RNA seq data
     :param nr_of_clusters: integer, number of cluster that should be consdered
@@ -23,7 +29,7 @@ def hierarchical_clustering(input_dataset, nr_of_clusters, cluster_method, linka
     pandas dataframe: input_dataframe (input dataframe with calculated cluster)
     integer: nr_of_clusters (number of clusters)
     function: ccl_fun (used function)
-    list: positional_arguments  
+    list: positional_arguments
     dictionary: keyword_arguments
     """
     dataset = input_dataset
@@ -32,7 +38,7 @@ def hierarchical_clustering(input_dataset, nr_of_clusters, cluster_method, linka
         raise ValueError("The number of clusters should an integer.")
     else:
         pass
-    
+
     check_value_type2 = isinstance(input_dataset, pd.DataFrame)
     if check_value_type2 is False:
         raise ValueError("The dataset should be a pandas dataframe.")
@@ -44,23 +50,23 @@ def hierarchical_clustering(input_dataset, nr_of_clusters, cluster_method, linka
         raise ValueError("The cluster method should be a string.")
     else:
         pass
-    
+
     if cluster_method == "KMeans":
         cluster = KMeans(n_clusters=nr_of_clusters)
         dataset['cluster'] = cluster.fit_predict(input_dataset)
         ccl_fun = KMeans
     else:
         pass
-    
+
     if cluster_method == "Hierarchical":
-        cluster = AgglomerativeClustering(n_clusters= nr_of_clusters, affinity='euclidean', linkage=linkage_method)
+        cluster = AgglomerativeClustering(n_clusters= nr_of_clusters,
+                                          affinity='euclidean',
+                                          linkage=linkage_method)
         dataset['cluster'] = cluster.fit_predict(dataset)
-        ccl_fun = AgglomerativeClustering 
-        
+        ccl_fun = AgglomerativeClustering
+
     positional_arguments = []
-    keyword_arguments = {'n_clusters': nr_of_clusters, 'affinity': 'euclidean','linkage': 'ward'}
-    
+    keyword_arguments = {'n_clusters': nr_of_clusters, 'affinity': 'euclidean',
+                         'linkage': 'ward'}
+
     return dataset, nr_of_clusters, ccl_fun, positional_arguments, keyword_arguments
-
-
-
